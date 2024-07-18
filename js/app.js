f3352673083293f94668b132657326b88374b657
alledpaiva <alled@freedomfi.com>
2024-07-18 19:23:59 -0300

    feat: JavaScript basic

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const personName = "Grimmjow";
+console.log(`Hello ${personName}, welcome to JS `);
+
+
+const numA = 11;
+const numB = 32;
+
+const total = numA + numB;
+
+console.log("The Total is :" + total);
+
+//prettier.ignore
+
+const nameList = [
+    'GrimJow',
+    'Luffy',
+    'Goku',
+    'Saitama'
+]
+
+function print(text){
+    console.log(text);
+}
+
+const xyz = print;
+console.log("😁" , xyz.name);
+
+nameList.forEach(print);
